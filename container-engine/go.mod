1dbd90eaa75cfe99887ce540a1c3e1ce789e4b00
osp-wag <osp@whiteawaygroup.com>
2024-10-25 07:16:52 +0200

    update

---
+++ container-engine/go.mod
@@ -0,0 +1,3 @@
+module build-your-own/container-engine
+
+go 1.22.3
--- terminal-emulator/main.go
+++ terminal-emulator/main.go
@@ -1,53 +1,98 @@
 package main
 
 import (
+	"bufio"
 	"os"
 	"os/exec"
 	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"github.com/creack/pty"
 )
 
+// MaxBufferSize sets the size limit
+// for our command output buffer.
+const MaxBufferSize = 16
+
 func main() {
-	a := app.New()
-	w := a.NewWindow("sleight")
+	// Initialize application
+	application := app.New()
+	// Initialize window
+	window := application.NewWindow("germ")
 
-	ui := widget.NewTextGrid() // Create a new TextGrid
+	// Create a new TextGrid
+	ui := widget.NewTextGrid()
 
-	bash := exec.Command("/bin/bash")
-	pty, err := pty.Start(bash)
+	// Spawn a bash subprocess
+	os.Setenv("TERM", "dumb")
+	cmd := exec.Command("/bin/bash")
 
-	if err != nil {
-		fyne.LogError("Failed to open pty", err)
-		os.Exit(1)
-	}
+	// Start pseudoterminal
+	PTY, _ := pty.Start(cmd)
 
-	defer bash.Process.Kill()
+	// Close the bash process when we close our process
+	defer cmd.Process.Kill()
 
-	pty.Write([]byte("ls\r"))
-	time.Sleep(1 * time.Second)
-	b := make([]byte, 1024)
-	_, err = pty.Read(b)
-	if err != nil {
-		fyne.LogError("Failed to read pty", err)
-	}
-	// s := fmt.Sprintf("read bytes from pty.\nContent:%s",  string(b))
-	ui.SetText(string(b))
+	// On key event
+	window.Canvas().SetOnTypedKey(func(event *fyne.KeyEvent) {
+		if event.Name == fyne.KeyEnter || event.Name == fyne.KeyReturn {
+			PTY.Write([]byte{'\r'})
+		}
+	})
 
-	// Create a new container with a wrapped layout
-	// set the layout width to 420, height to 200
-	w.SetContent(
-		container.New(
+	// On rune event
+	window.Canvas().SetOnTypedRune(func(r rune) {
+		PTY.WriteString(string(r))
+	})
+
+	buffer := [][]rune{}
+	reader := bufio.NewReader(PTY)
+
+	// Goroutine that reads from PTY
+	go func() {
+		line := []rune{}
+		buffer = append(buffer, line)
+		for {
+			r, _, _ := reader.ReadRune()
+
+			line = append(line, r)
+			buffer[len(buffer)-1] = line
+			if r == '\n' {
+				if len(buffer) > MaxBufferSize {
+					buffer = buffer[1:]
+				}
+
+				line = []rune{}
+				buffer = append(buffer, line)
+			}
+
+		}
+	}()
+
+	// Goroutine that renders to UI
+	go func() {
+		for {
+			time.Sleep(10 * time.Millisecond)
+
+			var lines string
+			for _, line := range buffer {
+				lines = lines + string(line)
+			}
+			ui.SetText(string(lines))
+		}
+	}()
+
+	// Create a new container with a wrapped layout and show itd
+	window.SetContent(
+		fyne.NewContainerWithLayout(
 			layout.NewGridWrapLayout(fyne.NewSize(420, 200)),
 			ui,
 		),
 	)
 
-	w.ShowAndRun()
+	window.ShowAndRun()
 
 }
